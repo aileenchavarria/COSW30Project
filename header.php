6943e03b9eb1c5fbabd94fc163fc2ca5edd04baa
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-10 15:06:41 -0700

    Delete header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<nav class="navigation">
-      <div class="navbar-right">
-        <a href="http://aileenc.sgedu.site/">Home</a>
-    <a href="http://aileenc.sgedu.site/register.php">Register</a>
-    <a href="http://aileenc.sgedu.site/login.php">Login</a>
-    <a href="http://aileenc.sgedu.site/userlist.php">Members</a>
-    <a href="http://aileenc.sgedu.site/departmentlist.php">Departments</a>
-      </div>
-    </nav>
