f1475719e20d7749c403447219793352aa81ea04
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-10 15:08:49 -0700

    Create login.php

---
+++ code/login.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>User Login Form</title>
+    <link href="othercss.css" rel="stylesheet" type="text/css" />
+    <script src="https://kit.fontawesome.com/50963e1c2a.js" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+<?php include 'header.php'; ?>
+<div class="post-content">
+<main>
+<h1>K E E N</h1>
+
+<?php include 'inc_login.php'; ?>
+</main>
+</div>
+
+
+<?php include 'footer.php'; ?>
+
+
+
+  </body>
+  </html>
