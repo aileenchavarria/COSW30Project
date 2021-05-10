2d4326d3fc0145ed88e92bb80316ad9a0ba96f9f
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-10 14:57:57 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<nav class="navigation">
+      <div class="navbar-right">
+        <a href="http://aileenc.sgedu.site/">Home</a>
+    <a href="http://aileenc.sgedu.site/register.php">Register</a>
+    <a href="http://aileenc.sgedu.site/login.php">Login</a>
+    <a href="http://aileenc.sgedu.site/userlist.php">Members</a>
+    <a href="http://aileenc.sgedu.site/departmentlist.php">Departments</a>
+      </div>
+    </nav>
