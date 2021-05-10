5d55430f80d7336070582dfe27211c3cc77835d1
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-10 14:58:26 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"  content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>K E E N - DATING WEBSITE</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+<header class="v-header container">
+<div class="fullscreen-video-wrap">
+ <video src="couple.mp4" autoplay="true" loop="true" muted="true" width="1000"></video>   
+</div>
+<div class="header-overlay"></div>
+
+<?php include 'header.php'; ?>
+
+
+<div class="header-content">
+<h1 id="header">K E E N</h1>
+<p>There's Someone for Everyone</p>
+</div>
+</header>
+
+
+
+<?php include 'footer.php'; ?>
+
+
+
+
+<script src="script.js"></script>
+  </body>
+</html>
