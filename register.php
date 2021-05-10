76a94a6b00d80e2c8b37d09405c52f51a318f7e9
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-10 15:00:11 -0700

    Create register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<DOCTYPE! html>
+  <head>
+    <title>User Registration Form</title>
+    <link href="othercss.css" rel="stylesheet" type="text/css" />
+    <script src="https://kit.fontawesome.com/50963e1c2a.js" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+<?php include 'header.php'; ?>
+<div class="post-content">
+<main>
+<h1>K E E N</h1>
+
+<?php include 'inc_registration.php'; ?>
+</main>
+</div>
+
+<?php include 'footer.php'; ?>
+
+  </body>
+  </html>
