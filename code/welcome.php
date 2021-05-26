cfec184a254008ff18c6e9b85eff10664e48d332
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-26 01:09:36 -0700

    Create welcome.php

---
+++ code/welcome.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"  content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>K E E N - DATING WEBSITE</title>
+    <link href="othercss.css" rel="stylesheet" type="text/css">
+  </head>
+
+
+
+  <body>
+<?php include 'header.php'; ?>
+<div class="post-content">
+<main>
+
+<center>
+<h2>Thanks for joining!</h2>
+<img id="header" src="https://i.ibb.co/B2v0dFy/heartr.gif">
+<p>
+You have been registered! Would you like to <a href='profile.php?id=" .$row['user_id'] ."'>see your profile</a>?
+Or <a href="http://aileenc.sgedu.site/userlist.php">view other users</a>?
+
+</main>
+</div>
+
+
+<?php include 'footer.php'; ?>
+
+
+
+  </body>
+  </html>
+</html>
