94b3e96d08c0b6ed08ba16e82a56f048474bb9ac
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-05-26 01:06:12 -0700

    Update header.php

--- code/header.php
+++ code/header.php
@@ -3,7 +3,6 @@
         <a href="http://aileenc.sgedu.site/">Home</a>
     <a href="http://aileenc.sgedu.site/register.php">Register</a>
     <a href="http://aileenc.sgedu.site/login.php">Login</a>
-    <a href="http://aileenc.sgedu.site/userlist.php">Members</a>
-    <a href="http://aileenc.sgedu.site/departmentlist.php">Departments</a>
+    <a href="http://aileenc.sgedu.site/userlist.php">Directory</a>
       </div>
     </nav>
