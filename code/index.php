5fe7c347f0e60e6b7609bd59fb00619bbe03818c
aileenchavarria <83977993+aileenchavarria@users.noreply.github.com>
2021-06-08 13:24:07 -0700

    Update index.php

--- code/index.php
+++ code/index.php
@@ -4,7 +4,7 @@
     <meta name="viewport"  content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>K E E N - DATING WEBSITE</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="istyle.css" rel="stylesheet" type="text/css">
   </head>
   <body>
 <header class="v-header container">
